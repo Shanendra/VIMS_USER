192ea4b08dffe6b9cf760faf4bb0046d6a4daa73
shanu singh <singhshanu204@gmail.com>
2024-04-22 04:40:53 +0530

    new notice

---
+++ app/src/main/java/com/example/vims_user/FullimageView.java
@@ -0,0 +1,40 @@
+package com.example.vims_user;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+
+public class FullimageView extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_fullimage_view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        imageView = findViewById(R.id.imageView);
+        String image  = getIntent().getStringExtra("image");
+
+        try {
+
+                Glide.with(this).load(image).into(imageView);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
